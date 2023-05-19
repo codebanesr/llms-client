d1d8521c571af5933dd51d9143e11189f3282376
Shanur Rahman <shanur.cse.nitap@gmail.com>
2023-05-19 17:04:22 +0300

    Adding bard completion as well

---
+++ src/adapters/bard.adapter.ts
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import { CompletionService } from '../interfaces/completion.interface';
+
+export class ClaudeAIAdapter implements CompletionService {
+  private readonly apiUrl: string;
+  private readonly apiKey: string;
+
+  constructor(apiUrl: string, apiKey: string) {
+    this.apiUrl = apiUrl;
+    this.apiKey = apiKey;
+  }
+
+  async complete(prompt: string, maxTokens: number): Promise<string> {
+    try {
+      axios.defaults.headers.common.Authorization = `Bearer ${this.apiKey}`;
+      axios.defaults.headers.common['Content-Type'] = `application/json`;
+
+      const response = await axios.post<BardAiResponse>(this.apiUrl, {
+        prompt,
+        maxTokens,
+      });
+
+      if (response.status === 200) {
+        return response.data.completion;
+      } else {
+        throw new Error(`Error completing prompt: ${response.status}`);
+      }
+    } catch (error) {
+      console.error('Error completing prompt:', error);
+      throw error;
+    }
+  }
+}
+
+interface BardAiResponse {
+  completion: string;
+}
